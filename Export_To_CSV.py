319b1d0be84c12185f78521896be62eceec659d0
ABZB <abzb13@gmail.com>
2025-06-23 21:43:13 -0400

    build initial egg array
    copy paste of original, with variable changes

--- Export_To_CSV.py
+++ Export_To_CSV.py
@@ -8,8 +8,39 @@ def make_eggs_list(pokearray, underlying_source_index = 0, ):
 
     max_personal = len(pokearray.write_array)
 
+    #build stage 1
+    for index, pokemon_data in enumerate(pokearray.write_array):
+        #Egg Moves. 
+        #if is a base forme, same as index
+        egg_index = 0
         if(index <= pokearray.max_species_index):
             egg_index = index
+        #XY/ORAS have all formes share the same egg move file
+        elif(pokearray.game in {'XY', 'ORAS'}):
+            #this gets the base species index from
+            egg_index = int(pokemon_data[1][2])
+        #otherwise we need to get the first two bytes of the base forme's egg move file, this is the pointer to the first alt forme's file. add (forme number - 1) to that to get correct file, IF IT EXISTS
+        else:
+            temp_value = from_little_bytes_int(pokearray.egg[int(pokemon_data[1][2])][0:2])
+
+            if(temp_value == int(pokemon_data[1][2]) or temp_value == 0):
+                pass
+            else:
+                egg_index = temp_value + int(pokemon_data[2][2]) - 1
+
+                #if the pointer in the new file is wrong, no eggs
+                if(from_little_bytes_int(pokearray.egg[egg_index][0:2]) != temp_value):
+                    egg_index = 0
+
+
+        #if length is 4 or less, than it has no actual egg moves
+
+        if(len(pokearray.egg[egg_index]) <= 4 or egg_index == 0):
+            pass
+        else:
+            #each move is 2 bytes. in XY/ORAS first two bytes are count of egg moves. In SM/USUM before those bytes are the pointer to the alt forme egg move file
+            for x in range((2 if pokearray.game in {'XY', 'ORAS'} else 4), len(pokearray.egg[egg_index]), 2):
+                pokearray.egg_array[index].append([index, 'Egg Move', '', pokearray.move_name_list[from_little_bytes_int(pokearray.egg[egg_index][x:x + 2])]])
 def build_total_output_array(pokearray, base_index = 0, target_index = 0, forme_number = 0):
 
     #iterate over all files
